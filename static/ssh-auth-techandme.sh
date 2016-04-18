3679861d02d9648d8c7a9222125bfa721bdfd465
Daniel Hansson <daniel@techandme.se>
2016-04-18 13:41:35 +0200

    change user + correct exit status

--- static/ssh-auth-techandme.sh
+++ static/ssh-auth-techandme.sh
@@ -7,7 +7,7 @@
 # Please select which user to grant access for Tech and Me by	#
 # typing the username of the user.				#
 # If the user doesn't exist, it will be created automatically.	#
-USER=root							#
+USER=techandme							#
 #################################################################
 
 USERPASS=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
@@ -42,7 +42,7 @@ if [ "$USER" = "root" ];
 then
 	mkdir -p /$USER/.ssh/
 	touch $AUTHFILEROOT
-cat << SSH_AUTH >> "$AUTHFILEROOT"
+	cat << SSH_AUTH >> "$AUTHFILEROOT"
 
 # Tech and Me
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiZsWBEV+XD5mWjZoljRFGgU7sViF9OC/RMKS2E4ew1EdzW98ffA8g+efD7hLHsQxWmUyhk248bSkTvXYICyNI77DFVS7Yc+9dcDD/hAn/FQxxyt8kZTfE7/ktZkP9BW2/HnQdNHiRVu5PvdaAUlwLbLlri+WKh/rUhAdSmJMXVE9ev1M2lH4VnG0Kxtau+cxTrxI+wuW+sGyYEnr1dv4eOV7EIvVxJx9nPpaHIHUwAAl+gqMFRuXQJ9tWPXAJ1O0oeFmfR0DzGLUo6RaLNgY9qEMf159o23sBZEuvRdz+TAf65GgSBylnpaut8c0qUxDJHPF2NDhZhVcM4vQ6Be/byIUYhFjosD2Hrixqi0SxjZdZyMczchalXh0LTuItQni4/M7sn707eBV/FiED/1RTMszqwppWalRdNFi5LklsEG0UVZAhIyccL0cQFcH+vkDNZB/elt4Ir55jjOQxCNuOhiRlohTNKg3vomb61gsmpI4Aen4a0yHVvJKuibBpTOPS0RdWGTIBIuFvL8fR7o24zGDh7g0mgoBkFj4mfRalRrcGG0aljCm86gFIOepyrXW+xO93z+9XI1oLv7YWb1TvdehczNrgrV/kjAGNH39Jssxod6E+9iQ3yJJre8TQQCORSmD8yH2GC6Lof1qijbrBzb9PXvOXGBksxULYukgoDw== techandme_ssh_auth
@@ -50,7 +50,7 @@ SSH_AUTH
 else
         mkdir -p /home/$USER/.ssh/
         touch $AUTHFILEUSER
-cat << SSH_AUTH >> "$AUTHFILEUSER"
+	cat << SSH_AUTH >> "$AUTHFILEUSER"
 
 # Tech and Me
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCiZsWBEV+XD5mWjZoljRFGgU7sViF9OC/RMKS2E4ew1EdzW98ffA8g+efD7hLHsQxWmUyhk248bSkTvXYICyNI77DFVS7Yc+9dcDD/hAn/FQxxyt8kZTfE7/ktZkP9BW2/HnQdNHiRVu5PvdaAUlwLbLlri+WKh/rUhAdSmJMXVE9ev1M2lH4VnG0Kxtau+cxTrxI+wuW+sGyYEnr1dv4eOV7EIvVxJx9nPpaHIHUwAAl+gqMFRuXQJ9tWPXAJ1O0oeFmfR0DzGLUo6RaLNgY9qEMf159o23sBZEuvRdz+TAf65GgSBylnpaut8c0qUxDJHPF2NDhZhVcM4vQ6Be/byIUYhFjosD2Hrixqi0SxjZdZyMczchalXh0LTuItQni4/M7sn707eBV/FiED/1RTMszqwppWalRdNFi5LklsEG0UVZAhIyccL0cQFcH+vkDNZB/elt4Ir55jjOQxCNuOhiRlohTNKg3vomb61gsmpI4Aen4a0yHVvJKuibBpTOPS0RdWGTIBIuFvL8fR7o24zGDh7g0mgoBkFj4mfRalRrcGG0aljCm86gFIOepyrXW+xO93z+9XI1oLv7YWb1TvdehczNrgrV/kjAGNH39Jssxod6E+9iQ3yJJre8TQQCORSmD8yH2GC6Lof1qijbrBzb9PXvOXGBksxULYukgoDw== techandme_ssh_auth
@@ -89,10 +89,10 @@ then
         echo $LANIP
         echo "A guide on how to open ports can be found here: https://goo.gl/Uyuf65"
         echo
+        exit 1
 else
         echo -e "\e[92m"
         echo "Port 22 is open, Tech and Me can now connect."
         echo -e "\e[0m"
+	exit 0
 fi
-
-exit 0
