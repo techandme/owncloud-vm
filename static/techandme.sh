8b049d127fa6d90478d69c39eb1222450a09be91
Daniel Hansson <daniel@techandme.se>
2016-04-30 18:40:06 +0200

    Errors fixed in new version of the VM

--- static/techandme.sh
+++ static/techandme.sh
@@ -10,6 +10,4 @@ echo
 echo "WAN IP: $WANIP"
 echo "LAN IP: $ADDRESS"
 echo
-echo "There is a bug in the ownCloud Core that breaks the password change with the occ command during setup."
-echo "This will hopefully be fixed in 9.0.2, please have patience and thank you for understanding."
 exit 0
