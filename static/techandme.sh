da9fb96aab376ccb40112496c60d47e6b260dad2
Daniel Hansson <daniel@techandme.se>
2016-04-30 15:56:13 +0200

    temporary alert
    https://github.com/owncloud/core/pull/24343

--- static/techandme.sh
+++ static/techandme.sh
@@ -10,4 +10,6 @@ echo
 echo "WAN IP: $WANIP"
 echo "LAN IP: $ADDRESS"
 echo
+echo "There is a bug in the ownCloud Core that breaks the password change with the occ command during setup."
+echo "This will hopefully be fixed in 9.0.2, please have patience and thank you for understanding."
 exit 0
