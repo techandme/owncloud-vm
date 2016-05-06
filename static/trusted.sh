dc0536b1030bf103a8af8cc921b1ad780f7cdb91
enoch85 <enoch85@gmail.com>
2016-05-06 15:41:27 +0200

    Fix internal server error

--- static/trusted.sh
+++ static/trusted.sh
@@ -5,7 +5,11 @@ OCPATH=$HTML/owncloud
 ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 SCRIPTS=/var/scripts
 
+# Change config.php
 php $SCRIPTS/update-config.php $OCPATH/config/config.php 'trusted_domains[]' localhost ${ADDRESS[@]} $(hostname) $(hostname --fqdn) 2>&1 >/dev/null
-php $SCRIPTS/update-config.php $OCPATH/config/config.php overwrite.cli.url https://$ADDRESS/owncloud 2>&1 >/dev/null
+php $SCRIPTS/update-config.php $OCPATH/config/config.php overwrite.cli.url https://$ADDRESS/ 2>&1 >/dev/null
+
+# Change .htaccess accordingly
+sed -i "s|RewriteBase /owncloud|RewriteBase /|g" /var/www/owncloud/.htaccess
 
 exit 0
