58fa7f9b265407b5167c5017314d34c343bb4efe
enoch85 <enoch85@gmail.com>
2016-07-13 23:51:59 +0200

    9.1RC2

--- beta/owncloud_install.sh
+++ beta/owncloud_install.sh
@@ -4,7 +4,7 @@
 
 ## THIS IS FOR TESTING ##
 
-OCVERSION=9.1.0RC1
+OCVERSION=9.1.0RC2
 DOWNLOADREPO=https://download.owncloud.org/community/testing/owncloud-$OCVERSION
 # DOWNLOADREPODEB=https://download.owncloud.org/
 CONVER=v1.3.1.0
