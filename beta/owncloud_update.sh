b9bdd85d7df92ddf23a1cc24c39747220e3964f6
Daniel Hansson <daniel@techandme.se>
2016-10-22 22:03:10 +0200

    make update script more stable

--- beta/owncloud_update.sh
+++ beta/owncloud_update.sh
@@ -2,128 +2,147 @@
 #
 ## Tech and Me ## - ©2016, https://www.techandme.se/
 #
-# Tested on Ubuntu Server 14.04.
+# Tested on Ubuntu Server 14.04 & 16.04.
 #
-SCRIPTS=/var/scripts
-HTML=/var/www/html
-OCPATH=/var/www/owncloud
-DATA=/var/ocdata
-SECURE="$SCRIPTS/setup_secure_permissions_owncloud.sh"
-OCVERSION=9.0.1
-STATIC="https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/static"
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
+
+# Put your theme name here:
 THEME_NAME=""
 
-# Must be root
-[[ $(id -u) -eq 0 ]] || { echo "Must be root to run script, in Ubuntu type: sudo -i"; exit 1; }
+# Static values
+STATIC="https://raw.githubusercontent.com/enoch85/ownCloud-VM/master/static"
+SCRIPTS=/var/scripts
+OCPATH=/var/www/owncloud
+BACKUP=/var/OCBACKUP
+HTML=/var/www
+SECURE="$SCRIPTS/setup_secure_permissions_owncloud.sh"
 
-# Set secure permissions
-if [ -f $SECURE ];
+# Must be root
+[[ `id -u` -eq 0 ]] || { echo "Must be root to run script, in Ubuntu type: sudo -i"; exit 1; }
+
+# Check if aptitude is installed
+if [ $(dpkg-query -W -f='${Status}' aptitude 2>/dev/null | grep -c "ok installed") -eq 1 ]
 then
-        echo "Script exists"
+    echo "Aptitude installed"
 else
-        mkdir -p $SCRIPTS
-        wget $STATIC/setup_secure_permissions_owncloud.sh -P $SCRIPTS
+    apt-get install aptitude -y
 fi
 
 # System Upgrade
-apt-get update -q2
-aptitude full-upgrade -y
+sudo apt-get update -q2
+sudo aptitude full-upgrade -y
+echo
+echo "System is now upgraded, now the script will upgrade ownCloud."
+echo "Which version do you want to upgrade to? Type it like this: '9.1.2RC1'"
+read OCVERSION
 
-# Enable maintenance mode
-sudo -u www-data php $OCPATH/occ maintenance:mode --on
+echo "Upgrading to $OCVERSION in 15 seconds... Press CTRL+C to abort."
+echo "Disclamer: Tech and Me or ownCloud is not responsible for any dataloss"
+echo "Config files are backed up and $OCDATA isn't removed, but things could go wrong."
+sleep 15
 
 # Backup data
-rsync -Aaxv $DATA $HTML
-rsync -Aax $OCPATH/config $HTML
-rsync -Aax $OCPATH/themes $HTML
-rsync -Aax $OCPATH/apps $HTML
+rm -R $BACKUP
+mkdir -p $BACKUP
+echo "Backing up data..."
+rsync -Aax $OCPATH/config $BACKUP
+rsync -Aax $OCPATH/themes $BACKUP
+rsync -Aax $OCPATH/apps $BACKUP
 if [[ $? > 0 ]]
 then
-    echo "Backup was not OK. Please check $HTML and see if the folders are backed up properly"
-    exit
+    echo "Backup was not OK. Please check $BACKUP and see if the folders are backed up properly"
+    exit 1
 else
-		echo -e "\e[32m"
+    echo -e "\e[32m"
     echo "Backup OK!"
     echo -e "\e[0m"
 fi
 wget https://download.owncloud.org/community/testing/owncloud-$OCVERSION.tar.bz2 -P $HTML
 
-if [ -f $HTML/owncloud-$OCVERSION.tar.bz2 ];
+if [ -f $HTML/owncloud-$OCVERSION.tar.bz2 ]
 then
-        echo "$HTML/owncloud-latest.tar.bz2 exists"
+    echo "$HTML/owncloud-$OCVERSION.tar.bz2 exists"
 else
-        echo "Aborting,something went wrong with the download"
-   exit 1
+    echo "Aborting,something went wrong with the download"
+    exit 1
 fi
 
-if [ -d $OCPATH/config/ ]; then
-        echo "config/ exists" 
+if [ -d $BACKUP/config/ ]
+then
+    echo "$BACKUP/config/ exists"
 else
-        echo "Something went wrong with backing up your old ownCloud instance, please check in $HTML if data/ and config/ folders exist."
-   exit 1
+    echo "Something went wrong with backing up your old ownCloud instance, please check in $BACKUP if config/ folder exist."
+    exit 1
 fi
 
-if [ -d $OCPATH/themes/ ]; then
-        echo "themes/ exists" 
+if [ -d $BACKUP/themes/ ]
+then
+    echo "$BACKUP/themes/ exists"
 else
-        echo "Something went wrong with backing up your old ownCloud instance, please check in $HTML if data/ and config/ folders exist."
-   exit 1
+    echo "Something went wrong with backing up your old ownCloud instance, please check in $BACKUP if themes/ folder exist."
+    exit 1
 fi
 
-if [ -d $OCPATH/apps/ ]; then
-        echo "apps/ exists" 
+# Let the magic begin...
+if [ -d $BACKUP/apps/ ]
+then
+    echo "$BACKUP/apps/ exists, removing old ownCloud instance in 5 seconds..." && sleep 5
+    rm -rf $OCPATH
+    tar -xjf $HTML/owncloud-$OCVERSION.tar.bz2 -C $HTML
+    rm $HTML/owncloud-$OCVERSION.tar.bz2
+    cp -R $BACKUP/themes $OCPATH/
+    cp -R $BACKUP/config $OCPATH/
+    cp -R $BACKUP/apps $OCPATH/
+    bash $SECURE
+    sudo -u www-data php $OCPATH/occ maintenance:mode --off
+    sudo -u www-data php $OCPATH/occ upgrade
 else
-        echo "Something went wrong with backing up your old ownCloud instance, please check in $HTML if data/ and config/ folders exist."
-   exit 1
+    echo "Something went wrong with backing up your old ownCloud instance, please check in $BACKUP if apps/ folders exist."
+    exit 1
 fi
 
-if [ -d $DATA/ ]; then
-        echo "data/ exists" && sleep 2
-        rm -rf $OCPATH
-        tar -xjf $HTML/owncloud-$OCVERSION.tar.bz2 -C $HTML 
-        rm $HTML/owncloud-$OCVERSION.tar.bz2
-        cp -R $HTML/themes $OCPATH/ && rm -rf $HTML/themes
-        cp -Rv $HTML/data $DATA && rm -rf $HTML/data
-        cp -R $HTML/config $OCPATH/ && rm -rf $HTML/config
-        cp -R $HTML/apps $OCPATH/ && rm -rf $HTML/apps
-        bash $SECURE
-        sudo -u www-data php $OCPATH/occ maintenance:mode --off
-        sudo -u www-data php $OCPATH/occ upgrade
-else
-        echo "Something went wrong with backing up your old ownCloud instance, please check in $HTML if data/ and config/ folders exist."
-   exit 1
-fi
+# Change owner of $BACKUP folder to root
+chown -R root:root $BACKUP
 
 # Enable Apps
 sudo -u www-data php $OCPATH/occ app:enable calendar
 sudo -u www-data php $OCPATH/occ app:enable contacts
 sudo -u www-data php $OCPATH/occ app:enable documents
-sudo -u www-data php $OCPATH/occ app:enable external
-
-# Disable maintenance mode
-sudo -u www-data php $OCPATH/occ maintenance:mode --off
 
 # Increase max filesize (expects that changes are made in /etc/php5/apache2/php.ini)
 # Here is a guide: https://www.techandme.se/increase-max-file-size/
 VALUE="# php_value upload_max_filesize 513M"
 if grep -Fxq "$VALUE" $OCPATH/.htaccess
 then
-        echo "Value correct"
+    echo "Value correct"
 else
-        sed -i 's/  php_value upload_max_filesize 513M/# php_value upload_max_filesize 513M/g' $OCPATH/.htaccess
-        sed -i 's/  php_value post_max_size 513M/# php_value post_max_size 513M/g' $OCPATH/.htaccess
-        sed -i 's/  php_value memory_limit 512M/# php_value memory_limit 512M/g' $OCPATH/.htaccess
+    sed -i 's/  php_value upload_max_filesize 513M/# php_value upload_max_filesize 513M/g' $OCPATH/.htaccess
+    sed -i 's/  php_value post_max_size 513M/# php_value post_max_size 513M/g' $OCPATH/.htaccess
+    sed -i 's/  php_value memory_limit 512M/# php_value memory_limit 512M/g' $OCPATH/.htaccess
 fi
 
 # Set $THEME_NAME
 VALUE2="$THEME_NAME"
-if grep -Fxq "$VALUE2" $OCPATH/config/config.php
+if grep -Fxq "$VALUE2" "$OCPATH/config/config.php"
 then
-        echo "Theme correct"
+    echo "Theme correct"
 else
-        sed -i "s|'theme' => '',|'theme' => '$THEME_NAME',|g" $OCPATH/config/config.php
-        echo "Theme set"
+    sed -i "s|'theme' => '',|'theme' => '$THEME_NAME',|g" $OCPATH/config/config.php
+    echo "Theme set"
 fi
+
+# Set secure permissions
+FILE="$SCRIPTS/setup_secure_permissions_owncloud.sh"
+if [ -f $FILE ]
+then
+    echo "Script exists"
+else
+    mkdir -p $SCRIPTS
+    wget -q $STATIC/setup_secure_permissions_owncloud.sh -P $SCRIPTS
+    chmod +x $SCRIPTS/setup_secure_permissions_owncloud.sh
+fi
+sudo bash $SCRIPTS/setup_secure_permissions_owncloud.sh
 
 # Repair
 sudo -u www-data php $OCPATH/occ maintenance:repair
@@ -137,16 +156,16 @@ sudo update-grub
 
 # Write to log
 touch /var/log/cronjobs_success.log
-echo "OWNCLOUD UPDATE success-$(date +"%Y%m%d")" >> /var/log/cronjobs_success.log
+echo "OW
+LOUD UPDATE success-`date +"%Y%m%d"`" >> /var/log/cronjobs_success.log
 echo
 echo ownCloud version:
 sudo -u www-data php $OCPATH/occ status
 echo
 echo
-sleep 3
 
-# Set secure permissions again
-bash $SECURE
+# Disable maintenance mode again just to be sure
+sudo -u www-data php $OCPATH/occ maintenance:mode --off
 
 ## Un-hash this if you want the system to reboot
 # sudo reboot
