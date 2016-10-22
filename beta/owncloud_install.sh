6f67f9c49ea3c93b747583a7020440663edd21ba
Daniel Hansson <daniel@techandme.se>
2016-10-22 18:47:32 +0200

    set back rc.local to normal

--- beta/owncloud_install.sh
+++ beta/owncloud_install.sh
@@ -572,6 +572,27 @@ rm $SCRIPTS/install-redis-php-7.sh
 # Upgrade
 aptitude full-upgrade -y
 
+# Clean up rc.local
+echo "Writes to rc.local..."
+
+cat << RCLOCAL > "/etc/rc.local"
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+
+exit 0
+
+RCLOCAL
+
 # Cleanup
 echo "$CLEARBOOT"
 apt-get autoremove -y
