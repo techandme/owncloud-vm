d7ed13e884900f040a109a2f533e39248dbd1a78
Daniel Hansson <daniel@techandme.se>
2017-05-21 23:04:49 +0200

    use function

--- owncloud_install_production.sh
+++ owncloud_install_production.sh
@@ -33,9 +33,7 @@ echo "If the field after ':' is blank you are probably running as a pure root us
 echo "It's possible to install with root, but there will be minor errors."
 echo
 echo "Please create a user with sudo permissions if you want an optimal installation."
-wget -q "$STATIC"/adduser.sh
-bash adduser.sh
-rm -f adduser.sh
+run_static_script adduser
 
 # Check Ubuntu version
 echo "Checking server OS and version..."
