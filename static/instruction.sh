69d4ba343ed4ac6f3dc90e6806ab05b02467c1e6
Daniel Hansson <daniel@techandme.se>
2017-09-24 17:46:23 +0200

    Update instruction.sh

--- static/instruction.sh
+++ static/instruction.sh
@@ -16,22 +16,22 @@ debug_mode
 clear
 cat << INST1
 +-----------------------------------------------------------------------+
-| Thanks for downloading this ownCloud VM by the ownCloud Community!  |
+| Thanks for downloading this ownCloud VM by Tech and Me!  		|
 |                                                                       |
 INST1
 echo -e "|"  "${Green}To run the startup script type the sudoer password. This will either${Color_Off}  |"
-echo -e "|"  "${Green}be the default ('owncloud') or the one chosen during installation.${Color_Off}   |"
+echo -e "|"  "${Green}be the default ('owncloud') or the one chosen during installation.${Color_Off}    |"
 cat << INST2
 |                                                                       |
 | If you have never done this before you can follow the complete        |
 | installation instructions here: https://goo.gl/JVxuPh                 |
 |                                                                       |
-| You can schedule the ownCloud update process using a cron job.       |
+| You can schedule the ownCloud update process using a cron job.        |
 | This is done using a script built into this VM that automatically     |
-| updates ownCloud, sets secure permissions, and logs the successful   |
+| updates ownCloud, sets secure permissions, and logs the successful    |
 | update to /var/log/cronjobs_success.log                               |
 | Detailed instructions for setting this up can be found here:          |
-| https://www.techandme.se/owncloud-update-is-now-fully-automated/     |
+| https://www.techandme.se/nextcloud-update-is-now-fully-automated/     |
 |                                                                       |
 |  ####################### Tech and Me - 2017 ########################  |
 +-----------------------------------------------------------------------+
